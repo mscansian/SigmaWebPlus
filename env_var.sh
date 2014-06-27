979ca30adcfcfc89b7141ca574d12a780b2aa122
mscansian <mscansian@gmail.com>
2014-06-27 19:48:06 +0200

    modificado o target para api 8 do android

--- env_var.sh
+++ env_var.sh
@@ -1,5 +1,5 @@
-export ANDROIDSDK=~/Development/adt-bundle-linux-x86_64-20140321/sdk
+export ANDROIDSDK=~/Development/android-adt-bundle/sdk
 export ANDROIDNDK=~/Development/android-ndk-r9d
-export ANDROIDAPI=19
+export ANDROIDAPI=8
 export ANDROIDNDKVER=r9
 export PATH=$ANDROIDNDK:$ANDROIDSDK/platform-tools:$ANDROIDSDK/tools:$PATH
