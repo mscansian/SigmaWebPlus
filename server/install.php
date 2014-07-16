8016dcc25bf31e1e7afc5c893e149cb756815cb1
mscansian <mscansian@gmail.com>
2014-07-16 21:08:34 +0200

    added CREATE TABLE

--- server/install.php
+++ server/install.php
@@ -3,6 +3,8 @@ define('VALID_ENTRY_POINT', '1');
 date_default_timezone_set('America/Sao_Paulo');
 
 require 'config.php';
+require 'database.php';
+
 if (!isset($CONFIG['SYSTEM']['install_time']))
 {
 	if ($_SERVER['REQUEST_METHOD']=="GET")
@@ -32,33 +34,74 @@ if (!isset($CONFIG['SYSTEM']['install_time']))
 	}
 	elseif ($_SERVER['REQUEST_METHOD']=="POST")
 	{
-			//Inicia a instalacao
-			$contents = "<?php\n";
-			$contents .= 'defined(\'VALID_ENTRY_POINT\') or die(\'\');'."\n";
-			foreach($_POST as $key => $value)
-			{
-				$key_array = explode('_', $key);
-				$contents .= '$CONFIG[\''.$key_array[0].'\'][\''.$key_array[1].'\'] = \''.$value.'\';'."\n";
-			}
-			$contents .= '$CONFIG[\'SYSTEM\'][\'install_time\'] = \''.time().'\';'."\n";
-			$contents .= "?>";
-			
-			$config_file = fopen('config.php', 'w');
-			if ($config_file)
-			{
-				fwrite($config_file, $contents);
-				fclose($config_file);
-				echo '<h2>Instalacao realizada com sucesso!';
-			}
-			else
-			{
-				echo '<h2 style="background-color:red;">Erro: Nao foi possivel escrever no arquivo config.php. Verifique as permissoes do arquivo.</h2>';
-			}
+		//Inicia a instalacao
+		$contents = "<?php\n";
+		$contents .= 'defined(\'VALID_ENTRY_POINT\') or die(\'\');'."\n";
+		foreach($_POST as $key => $value)
+		{
+			$key_array = explode('_', $key);
+			$contents .= '$CONFIG[\''.$key_array[0].'\'][\''.$key_array[1].'\'] = \''.$value.'\';'."\n";
+		}
+		$contents .= '$CONFIG[\'SYSTEM\'][\'install_time\'] = \''.time().'\';'."\n";
+		$contents .= "?>";
 		
+		$config_file = fopen('config.php', 'w');
+		if ($config_file)
+		{
+			fwrite($config_file, $contents);
+			fclose($config_file);
+			require 'config.php';
+		}
+		else
+		{
+			die ('<h2 style="background-color:red;">Erro: Nao foi possivel escrever no arquivo config.php. Verifique as permissoes do arquivo.</h2>');
+		}
+		
+		$sqlquery1 = 'DROP TABLE IF EXISTS users, requests, cache;';
+		$sqlquery2 = 'CREATE TABLE users (
+			matricula INT(6) NOT NULL PRIMARY KEY,
+			nome VARCHAR(70) NOT NULL,
+			centro VARCHAR(20) NOT NULL,
+			tipo VARCHAR(50) NOT NULL
+		);';
+		
+		$sqlquery3 = 'CREATE TABLE requests (
+			matricula INT(6) NOT NULL,
+			data TIMESTAMP NOT NULL,
+			hash VARCHAR(32) NULL,
+			newhash VARCHAR(32) NULL,
+			ip VARCHAR(16) NOT NULL,
+			versao VARCHAR(16) NOT NULL,
+			error BOOLEAN NOT NULL,
+			notes VARCHAR(50) NULL,
+			invalidcredentials BOOLEAN NOT NULL,
+			fromcache BOOLEAN NOT NULL
+		);';
+		
+		$sqlquery4 = 'CREATE TABLE cache (
+			matricula INT(6) NOT NULL PRIMARY KEY,
+			data TIMESTAMP NOT NULL,
+			dados TEXT NOT NULL
+		);';
+		
+		$database = new cDatabase();
+		$result = $database->query($sqlquery1);
+		$result = $result && $database->query($sqlquery2);
+		$result = $result && $database->query($sqlquery3);
+		$result = $result && $database->query($sqlquery4);
+		if ($result)
+		{
+			echo '<h2>Instalacao realizada com sucesso!';
+		}
+		else
+		{
+			die ('<h2 style="background-color:red;">Erro: Nao foi possivel criar as tabelas no banco de dados</h2>');
+		}
+		$database->disconnect();
 	}
 }
 else
 {
-	echo '<h2 style="background-color:red;">Erro: Instalacao ja realizada em '.date("d/m/Y H:i:s",$CONFIG['SYSTEM']['install_time']).'</h2>';
+	die('<h2 style="background-color:red;">Erro: Instalacao ja realizada em '.date("d/m/Y H:i:s",$CONFIG['SYSTEM']['install_time']).'</h2>');
 }
 ?>
